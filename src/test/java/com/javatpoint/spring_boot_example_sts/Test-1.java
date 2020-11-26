366e39889fda053fff6543509b8c7984f0929fe7
Your Name <you@example.com>
2020-11-26 15:02:34 +0530

    changes done

---
+++ src/test/java/com/javatpoint/spring_boot_example_sts/Test-1.java
@@ -0,0 +1,14 @@
+package com.javatpoint.spring_boot_example_sts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMvcApplicationTests {
+
+	@Test
+
+	void contextLoads() {
+	}
+
+}
